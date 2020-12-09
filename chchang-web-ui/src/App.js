025f9839f39a1ba413a2b27da1cecd7664d7948e
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-10 02:12:51 +0800

    update ui code

---
+++ chchang-web-ui/src/App.js
@@ -0,0 +1,23 @@
+import styles from './App.module.scss';
+
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <div className={styles.App.header}>
+
+      </div>
+      <div className={styles.content}>
+        <div></div>
+        <div></div>
+        <div></div>
+
+      </div>
+      <div className={styles.App.footer}>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
