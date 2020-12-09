0993622eabeafd2a2afdddcd30fd830461117fbe
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-09 23:15:40 +0800

    update server

--- src/chchang-web/server.cpp
+++ src/chchang-web/server.cpp
@@ -36,42 +36,30 @@ std::string mime_type(boost::beast::string_view path)
     if(iequals(ext, ".svgz")) return "image/svg+xml";
     return "application/text";
 }
+static auto get_static(const std::string& from_shared) {
+  auto pathfs = avalon::app::install_dir() / "share" / from_shared;
+  auto rep = pixiu::make_response(pathfs);
+  rep.apply([&pathfs](auto&& frep){
+    frep.set(http::field::content_type, mime_type(pathfs.string()));
+  });
+  return rep;
+}
 int main(int argc, char* argv[]) {
   pixiu::logger::config(avalon::app::install_dir() / "etc" / "config.json");
   /**
    * make a http server and listen to 8080 port
    */
   auto server = pixiu::make_server();
-  server.get("/", [](const auto& req) -> pixiu::server_bits::response {
-    pixiu::logger::get("app").debug("root target: {}", req.target().to_string());
-    http::response<http::string_body> rep;
-    rep.body() = "hello world";
-    return pixiu::server_bits::response(rep);
+  server.get("/hello_world", [](const auto& req) {
+    return pixiu::make_response("hello world");
   });
-  server.get("/static/.+", [](const auto& req) -> pixiu::server_bits::response {
-    pixiu::logger::get("app").debug("static target: {}", req.target().to_string());
-    auto pathfs = avalon::app::install_dir() / "share" / req.target().substr(8).to_string();
-    auto path = pathfs.string();
-    boost::beast::error_code ec;
-    http::file_body::value_type body;
-    body.open(
-      path.c_str(), 
-      boost::beast::file_mode::scan, 
-      ec
-    );
-    if(ec) {
-      throw pixiu::server_bits::error::server_error(ec.message());
-    }
-    const auto fsize = body.size();
-    http::response<http::file_body> rep {
-      std::piecewise_construct,
-      std::make_tuple(std::move(body)),
-      std::make_tuple(http::status::ok, req.version())
-    };
-    rep.set(http::field::content_type, mime_type(path));
-    rep.content_length(fsize);
-    return pixiu::server_bits::response(std::move(rep));
+  server.get("/static/.+", [](const auto& req) {
+    return get_static(req.target().substr(8).to_string());
+  });
+  server.get("/", [](const auto& req) {
+    return get_static("index.html");
   });
   server.listen("0.0.0.0", std::atoi(argv[1]));
+
   server.run();
 }
