9c37c1cb7d26a149cbd062be003905239977cefb
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-11 01:43:15 +0800

    ui layout, basic function define

--- chchang-web-ui/src/App.js
+++ chchang-web-ui/src/App.js
@@ -4,17 +4,20 @@ import styles from './App.module.scss';
 const App = () => {
   return (
     <div className={styles.App}>
-      <div className={styles.App.header}>
-
+      <div className={styles.Header}>
+        home, login
       </div>
-      <div className={styles.content}>
-        <div></div>
-        <div></div>
-        <div></div>
-
+      <div className={styles.LSidebar}>
+        tree view
       </div>
-      <div className={styles.App.footer}>
-
+      <div className={styles.Content}>
+        content view: markdown viewer, canvas, dialog
+      </div>
+      <div className={styles.RSidebar}>
+        content index
+      </div>
+      <div className={styles.Footer}>
+        copyright, status, info
       </div>
     </div>
   );
