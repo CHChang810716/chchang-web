6a39fd6a0650096ab85abc88756c892431e23983
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-16 01:15:50 +0800

    better look remove fake code

--- chchang-web-ui/src/ModelCtrl/Article.js
+++ chchang-web-ui/src/ModelCtrl/Article.js
@@ -1,46 +1,8 @@
 import EventEmitter from 'events'
-import makeid from '../makeid'
-const fakeArticle = (i) => {
-  const str =  `
-# Article${i}
-
-## h2 is this
-
-### h3 is this
-
-#### h4 is this
-
-* markdown viewer
-* canvas
-* dialog
-
-## h2 is this
-
-## h2 is this
-
-## h2 is this
-
-## h2 is this
-
-## h2 is this
-
-## h2 is this
-
-## h2 is this
-
-`;
-  return str;
-}
 class Article {
   constructor(api) {
     this.index = []
     this.articles = []
-    for(let i = 0; i < 30; i ++) {
-      this.index.push({
-        gid: makeid(8),
-        label: `Article ${i}`
-      })
-    }
     this.currFetchedIter = 0;
     this.eventsCtrl = new EventEmitter();
   }
@@ -54,15 +16,6 @@ class Article {
       num = 3;
     }
     console.log('fetchMore')
-    setTimeout(()=>{
-      for(let i = 0; i < num && this.currFetchedIter < this.index.length; i ++) {
-        const str = fakeArticle(this.currFetchedIter);
-        this.currFetchedIter++;
-        this.articles.push({content: str})
-      }
-      console.log('emit onFetch')
-      this.eventsCtrl.emit('fetched', this.articles);
-    })
   }
   hasMore = () => {
     console.log('hasMore')
