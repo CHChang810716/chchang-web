1cc4f1d24f263a85f93b660d6833fbad5fb0efb9
Chia-Hua Chang <CHChang810716@gmail.com>
2020-12-16 03:43:07 +0800

    update server API

--- src/chchang-web/server.cpp
+++ src/chchang-web/server.cpp
@@ -1,6 +1,7 @@
 #include <pixiu/server.hpp>
 #include <pixiu/response.hpp>
 #include <avalon/app/path.hpp>
+#include <nlohmann/json.hpp>
 using namespace boost::beast;
 
 std::string mime_type(boost::beast::string_view path)
@@ -47,6 +48,13 @@ static auto get_static(const std::string& from_shared) {
   });
   return rep;
 }
+auto& logger() {
+  return pixiu::logger::get("app");
+}
+
+template<class... T>
+using params = pixiu::server_bits::params<T...>;
+
 int main(int argc, char* argv[]) {
   pixiu::logger::config(avalon::app::install_dir() / "etc" / "config.json");
   /**
@@ -59,10 +67,35 @@ int main(int argc, char* argv[]) {
   server.get("/api/v1/hello_world", [](const auto& req) {
     return pixiu::make_response("hello world");
   });
-  server.get("/.+", [](const auto& req) {
-    return get_static(req.target().substr(1).to_string());
-  });
-  server.listen("0.0.0.0", std::atoi(argv[1]));
-
-  server.run();
+  server
+    .get("/api/v1/devlog/index", [](const auto& req) {
+      nlohmann::json index = std::vector<std::string>();
+      auto devlogfs = avalon::app::install_dir() / "devlog";
+      logger().debug("get index: {}", devlogfs.string());
+      boost::filesystem::directory_iterator end_iter;
+      for(boost::filesystem::directory_iterator iter (devlogfs); iter != end_iter; ++iter) {
+        auto curr_path = iter->path();
+        logger().debug("current scan: {}", curr_path.string());
+        if(boost::filesystem::is_regular_file(curr_path)) {
+          auto curr_name = curr_path.stem();
+          index.push_back(curr_name.string());
+        }
+      }
+      return pixiu::make_response(index.dump());
+    })
+    .get("/api/v1/devlog/article", params<std::string>("name"),
+      [](const auto& req, const std::string& name){
+        auto article_path = avalon::app::install_dir() 
+          / "devlog" 
+          / fmt::format("{}.md", name)
+        ;
+        logger().debug("get devlog article: {}", article_path.string());
+        return pixiu::make_response(article_path);
+      }
+    )
+    .get("/.+", [](const auto& req) {
+      return get_static(req.target().substr(1).to_string());
+    })
+    .listen("0.0.0.0", std::atoi(argv[1]))
+    .run();
 }
